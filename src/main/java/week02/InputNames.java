a0993e76cea41bae9211f57158d08dc6c3d26a23
GergelyMihalyi <33895800+GergelyMihalyi@users.noreply.github.com>
2020-11-05 10:26:16 +0100

    Órai feladat nevek

---
+++ src/main/java/week02/InputNames.java
@@ -0,0 +1,24 @@
+package week02;
+
+import java.util.Scanner;
+
+public class InputNames {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] arrayOfNames = new String[5];
+
+
+
+        for(int i = 0; i < 5; i++){
+            System.out.println("Please enter your name: ");
+            arrayOfNames[i]=scanner.nextLine();
+        }
+
+
+        for(int i = 0; i < arrayOfNames.length; i++){
+            System.out.println(arrayOfNames[i]);
+        }
+
+    }
+}
