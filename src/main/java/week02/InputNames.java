3274d29b3f88b2e67f7fec90a72b634290568b31
GergelyMihalyi <33895800+GergelyMihalyi@users.noreply.github.com>
2020-11-09 08:03:23 +0100

    Órai feladat programozási nyelvek formázás

--- src/main/java/week02/InputNames.java
+++ src/main/java/week02/InputNames.java
@@ -8,15 +8,12 @@ public class InputNames {
         Scanner scanner = new Scanner(System.in);
         String[] arrayOfNames = new String[5];
 
-
-
-        for(int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++) {
             System.out.println("Please enter your name: ");
-            arrayOfNames[i]=scanner.nextLine();
+            arrayOfNames[i] = scanner.nextLine();
         }
 
-
-        for(int i = 0; i < arrayOfNames.length; i++){
+        for (int i = 0; i < arrayOfNames.length; i++) {
             System.out.println(arrayOfNames[i]);
         }
 
