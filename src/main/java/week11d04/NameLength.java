69d94bea5fbb52747fe17266bfc6aa904eb62d6f
GergelyMihalyi <>
2021-01-14 10:17:39 +0100

    week10d04 NameLength without Test

---
+++ src/main/java/week11d04/NameLength.java
@@ -0,0 +1,21 @@
+package week11d04;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NameLength {
+
+    public List<Integer> getLength(List<String> names){
+        List<Integer> lengths = new ArrayList<>();
+
+        for(String name: names){
+            if(name.startsWith("J")){
+                if(!lengths.contains(name.length())){
+                    lengths.add(name.length());
+                }
+            }
+        }
+        return lengths;
+    }
+
+}
